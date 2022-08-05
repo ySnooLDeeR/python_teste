06fd02cd8b06ddd2961839bbb1a5a098a1da7666
Júlio César Duarte dos Santos <108158889+ySnooLDeeR@users.noreply.github.com>
2022-08-05 16:25:03 -0300

    exercicios de operadores

---
+++ python_teste/novo projeto/Operadores.py
@@ -0,0 +1,10 @@
+'''EXERCICIOS DE OPERADORES NUMERICOS
+OPERADORES --> '''
+
+A = 5
+B = 15
+C = 20
+
+print("A == B AND B > C : " , A == B and B > C )
+print("A < B OR B > C :" , A < B or B > C )
+print("not A == B : " , not A == B)
