081c0365d4a7d35884ca941ae12db9081a5dfe42
Júlio César Duarte dos Santos <108158889+ySnooLDeeR@users.noreply.github.com>
2022-08-05 16:51:50 -0300

    if else estrura simples

---
+++ python_teste/novo projeto/operador de decisao ( if ).py
@@ -0,0 +1,8 @@
+'''estrutura simples ( condicionais )'''
+
+idade = int(input("Qual a sua idade ?"))
+
+if idade >= 18 :
+        print("Sua idade é maior que 18 anos !")
+else :
+        print("Sua idade é menor que 18 anos !")
